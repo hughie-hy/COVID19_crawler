fff19f1a9cb1e68c2543c0eadb305fcacdcd5a3f
hughie-hy <58277694+hughie-hy@users.noreply.github.com>
2022-01-03 10:55:48 +0800

    Update and rename 06_正则表达式.py to 06_正则表达式常见语法.py

---
+++ 06_正则表达式常见语法.py
@@ -0,0 +1,22 @@
+import re
+
+rs = re.findall('\d','123')
+rs = re.findall('\d*','456')
+rs = re.findall('\d+','789')
+rs = re.findall('a+','aaabcd')
+
+print(rs)
+
+# 分组的使用
+rs = re.findall('\d{1,2}','chuan13zhi2')
+rs = re.findall('aaa(\d+)b','aaa91b')
+print(rs)
+
+# 一般的正则表达式匹配一个\需要四个\
+rs = re.findall('a\\\\bc','a\\bc')
+print(rs)
+print('a\\bc')
+
+# 使用r原串
+rs = re.findall(r'a\\rbc','a\\rbc')
+print(rs)
