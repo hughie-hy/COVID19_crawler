a71610baa302717547ada93dc874da51e9bbcba9
hughie-hy <58277694+hughie-hy@users.noreply.github.com>
2022-01-03 11:00:54 +0800

    Update 07_提取json字符串.py

--- 07_提取json字符串.py
+++ 07_提取json字符串.py
@@ -1,15 +1,16 @@
-import re
 import requests
 from bs4 import BeautifulSoup
-
+import re
 
 response = requests.get('https://ncov.dxy.cn/ncovh5/view/pneumonia')
 home_page = response.content.decode()
+#print(home_page)
 
 soup = BeautifulSoup(home_page,'lxml')
-script = soup.find(id='getListByCountryTypeService2true')
-text = script.string
-# print(text)
+script = soup.find(id = 'getListByCountryTypeService2true')
+st = script.string
 
-json_str = re.findall(r'\[.+\]',text)[0]
-print(json_str)
+json_string = re.findall(r'\[.+\]',st)[0]
+
+print(json_string)
+
